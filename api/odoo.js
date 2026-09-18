9d55ad27254c327212daec6284d6848024754854
Isaque Pavão <isaquemoises14@gmail.com>
2026-09-18 16:07:03 -0400

    Update odoo.js

--- api/odoo.js
+++ api/odoo.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
             });
         }
 
-        // 2. Procura de Produtos trazendo Descrição, Preço Venda, Custo e Estoque
+        // 2. Procura apenas produtos onde o tipo é Mercadorias ('consu' ou 'product')
         const prodRes = await fetch(ODOO_URL, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -52,7 +52,12 @@ export default async function handler(req, res) {
                         ODOO_API_KEY,
                         "product.template",
                         "search_read",
-                        [[["name", "ilike", query]]],
+                        [
+                            [
+                                ["name", "ilike", query],
+                                ["detailed_type", "in", ["consu", "product"]]
+                            ]
+                        ],
                         { 
                             fields: ["id", "name", "list_price", "standard_price", "qty_available"], 
                             limit: 50 
